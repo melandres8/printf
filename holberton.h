7d352cad58250c4b99d32700de32027769912e43
melandres8 <melanmosta@gmail.com>
2020-03-15 20:58:10 +0000

    Update

--- holberton.h
+++ holberton.h
@@ -1,12 +1,19 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-int _putchar(char c);
-int _printf(const char *format, ...);
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 typedef struct form
 {
 	char *formt;
 	int (*func)();
-}forms;
+} forms;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int func_char(va_list chr);
+int func_string(va_list str);
+
 #endif
