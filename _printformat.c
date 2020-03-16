27a72b15bd7ab737216d7d2411782068e8258970
dondropo <alejandroruscamoreno@gmail.com>
2020-03-16 23:07:57 +0000

    adding print format function

--- _printformat.c
+++ _printformat.c
@@ -23,3 +23,64 @@ static int (*validate_format(const char *format))(va_list)
 	}
 	return (formt[i].func);
 }
+
+/**
+ * _printf - Our own print format.
+ * @format: Differents formats.
+ * Return: Number of characters.
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0, counter = 0;
+	va_list list;
+	int (*func)(va_list);
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+
+	va_start(list, format);
+	
+	while (format[i] != '\0')
+	{
+		while (format[i] != '%' && format[i])
+		{
+			_putchar(format[i]);
+			counter++;
+			i++;
+		}
+		if (!format[i])
+		{
+			return (counter);
+		}
+
+		func = validate_format(&format[i + 1]);
+
+		if (func != NULL)
+		{
+			counter = counter + func(list);
+			i = i + 2;
+			continue;
+		}
+		if (!format[i + 1])
+		{
+			return (-1);
+		}
+
+		_putchar(format[i]);
+		counter++;
+
+		if (format[i + 1] == '%')
+		{
+			i = i + 2;
+		}
+		else
+		{
+			i++;
+		}
+		i++;
+	}
+	va_end(list);
+	return (counter);
+}
